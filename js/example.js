182c95213216ea541945fe3e1748debc0c2b62b3
EJ Serafin <edgardo@chainblx.com>
2020-02-22 14:06:40 -0800

    i found it! terminal is the business

--- js/example.js
+++ js/example.js
@@ -4,4 +4,8 @@ console.log(prompt);
 
 // don't like that at all. research if there is a way to start the cursor after the previously written code. 
 
-// terrible, this isn't going to work for me. drawback from using vi. or maybe i just need to research more to see how to get the damn cursor under previously written text 
+// terrible, this isn't going to work for me. drawback from using vi. or maybe i just need to research more to see how to get the damn cursor under previously written text
+
+// aha ha, finally found how to do it. 
+
+// ok I like vi now  
