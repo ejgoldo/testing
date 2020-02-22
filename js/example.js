4f320ec3ad7ff94c0e83cf9e2e9292576cf19a00
EJ Serafin <edgardo@chainblx.com>
2020-02-21 22:55:39 -0800

    tested with js file, its the same as txt. research required

--- js/example.js
+++ js/example.js
@@ -1,3 +1,5 @@
 const prompt = prompt("hello there this is your first JS code line created with the terminal");
 
 console.log(prompt);
+
+// don't like that at all. research if there is a way to start the cursor after the previously written code. 
