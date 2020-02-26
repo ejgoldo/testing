4ef7e452988b10156d9a311197ee441acbf743b5
EJ Serafin <edgardo@chainblx.com>
2020-02-25 16:18:38 -0800

    moved file out of js dir

---
+++ example.js
@@ -0,0 +1,11 @@
+const prompt = prompt("hello there this is your first JS code line created with the terminal");
+
+console.log(prompt);
+
+// don't like that at all. research if there is a way to start the cursor after the previously written code. 
+
+// terrible, this isn't going to work for me. drawback from using vi. or maybe i just need to research more to see how to get the damn cursor under previously written text
+
+// aha ha, finally found how to do it. 
+
+// ok I like vi now  
